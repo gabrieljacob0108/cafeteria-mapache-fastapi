89cea39d2a18fd884459cd470fcfa9879df3ddfe
gabrieljacob0108 <gabrieljacob4468@gmail.com>
2026-06-30 16:31:35 -0500

    Delete py.py

--- py.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-uri = "mongodb+srv://gabrieljacob:T1007158017@cluster0.w20wfrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-client = MongoClient(uri)
-
-print(client.list_database_names())
